120a408370f5368c775f24c75a2e4953e95e115d
Kamil Badowicz <kamotek123@gmail.com>
2025-04-25 22:16:57 +0200

    fix(tests): removed redundant code that caused test failure

--- BFFService/src/main/java/com/bffservice/application/query/handler/UserAuthQueryHandler.java
+++ BFFService/src/main/java/com/bffservice/application/query/handler/UserAuthQueryHandler.java
@@ -1,25 +1,25 @@
-package com.bffservice.application.query.handler;
-
-import com.bffservice.domain.model.AggregatedUser;
-import com.bffservice.domain.repository.UserAuthRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-@Component
-@RequiredArgsConstructor
-public class UserAuthQueryHandler {
-
-    private final UserAuthRepository userRepository;
-
-    public Optional<AggregatedUser> handle(UUID userId) {
-        return userRepository.findById(userId);
-    }
-
-    public List<AggregatedUser> handle() {
-        return userRepository.findAll();
-    }
-}
+//package com.bffservice.application.query.handler;
+//
+//import com.bffservice.domain.model.AggregatedUser;
+//import com.bffservice.domain.repository.UserAuthRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import java.util.UUID;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class UserAuthQueryHandler {
+//
+//    private final UserAuthRepository userRepository;
+//
+//    public Optional<AggregatedUser> handle(UUID userId) {
+//        return userRepository.findById(userId);
+//    }
+//
+//    public List<AggregatedUser> handle() {
+//        return userRepository.findAll();
+//    }
+//}
